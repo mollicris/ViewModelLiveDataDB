f278fce084b6a194cfe4b886a08ba6001e2f6bb5
crmollinedo <cristianm184@hotmail.com>
2022-09-13 22:57:27 -0400

    Data Binding Live Data

--- app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
+++ app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
@@ -17,12 +17,14 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this,viewModelFactory).get(MainActivityViewModel::class.java)
         binding.myViewModel = viewModel
+        binding.lifecycleOwner=this
+
        /* viewModel.totalData.observe(this, Observer {
             binding.resultTextView.text = it.toString()
-        })*/
+        })
 
         binding.insertButton.setOnClickListener {
             viewModel.setTotal(binding.inputEditText.text.toString().toInt())
-        }
+        }*/
     }
 }
--- app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
+++ app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
@@ -1,19 +1,26 @@
 package com.anushka.viewmodeldemo2
-
+import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal : Int) : ViewModel() {
      private var total = MutableLiveData<Int>()
+     val inputData = MutableLiveData<String>()
      val totalData : LiveData<Int>
      get() = total
 
+     //val numberChange : LiveData<String>
+    // get() = number
+
+
     init {
         total.value = startingTotal
+        //number.value = num
     }
 
-    fun setTotal(input:Int){
+    fun setTotal(){
+        val input = (inputData.value).toString().toInt()
         total.value =(total.value)?.plus(input)
     }
 }
